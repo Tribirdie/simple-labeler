e5843724a12a84c104570112b372ca727f6fbd4e
Estudiante <apaapf23@gmail.com>
2026-02-26 12:21:59 -0300

    add new json persistance

--- src/widget_funcs.py
+++ src/widget_funcs.py
@@ -1,8 +1,11 @@
 from pathlib import Path
+from pathlib import PurePath
 import os
-import cv2
 import tkinter
-from .utility_funcs import get_files_in_dir
+import json
+import cv2
+from .utility_funcs import get_files_in_dir, make_json, dump_dict_to_json, load_json_if_exists
+from .utility_funcs import json_entry_exists
 
 class WidgetFunctions:
     '''Central point for all the functions used in the gui'''
@@ -13,10 +16,13 @@ class WidgetFunctions:
         self.GUI = GUI 
         self.ext_allowed = (".png", ".jpg")
         self.file_entries = []
+
         self.file_label_statuses = {}
         self.amount_of_entries = 0
         self.current_file_index = 0 # tracks current file being operated on
         self.points = [] # holds the clicked that a rectangle will be drawn over
+
+        self.json_file = 1
         self.file_being_read = -1 # cv2 matrix
         self.backup_file_cpy = -1
     
@@ -26,7 +32,13 @@ class WidgetFunctions:
             self.points.append((x,y))
 
     def draw_square(self):
+        '''Draws a square. The changes can be rejected or approved'''
+        
+        if not os.path.exists(self.file_entries[self.current_file_index]):
+            return
+
         self.file_being_read = cv2.imread(self.file_entries[self.current_file_index])
+        # copy in case the user wants to undo the changes
         self.backup_file_cpy = cv2.imread(self.file_entries[self.current_file_index])
 
         cv2.namedWindow(self.file_entries[self.current_file_index], cv2.WINDOW_NORMAL)
@@ -36,7 +48,10 @@ class WidgetFunctions:
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-        cv2.rectangle(self.file_being_read, self.points[0], self.points[1], (0,0,255), 3)
+        try:
+            cv2.rectangle(self.file_being_read, self.points[0], self.points[1], (0,0,255), 3)
+        except IndexError:
+            pass
 
     def preview(self):
         '''preview the drawn image before applying the changes'''
@@ -46,18 +61,30 @@ class WidgetFunctions:
         cv2.destroyAllWindows()
 
     def choose_file(self):
+        '''Grab all the files in a directory and put them in the scrolledtext as a string'''
         directory_chosen = tkinter.filedialog.askdirectory()
+
+        get_last_dir = directory_chosen.split("/")
+        self.json_file = f"label_statuses/{get_last_dir[-1]}.json"
+        if not os.path.exists(self.json_file):
+            make_json(self.json_file)
+        else:
+            self.file_label_statuses = load_json_if_exists(self.json_file)
+
         files_n_dir, self.file_entries = get_files_in_dir(directory_chosen, self.ext_allowed, self.file_label_statuses)
         self.amount_of_entries = len(self.file_entries)
+        dump_dict_to_json(self.json_file, self.file_label_statuses)
 
         self.GUI.directory.set(files_n_dir)
         self.GUI.curr_file_name.set(self.file_entries[0])
         self.GUI.file_label_status.set(self.file_label_statuses[self.file_entries[0]])
 
+        # update scrolled text. clear it everytime to accomodate for new files.
         self.GUI.scroll_text.delete('1.0', tkinter.END)
         self.GUI.scroll_text.insert(tkinter.INSERT, self.GUI.directory.get());
 
     def lower_pos(self):
+        # to avoid negative indexing
         if self.current_file_index != 0:
             self.current_file_index -= 1
             self.GUI.curr_file_name.set(self.file_entries[self.current_file_index])
@@ -66,25 +93,22 @@ class WidgetFunctions:
     def incr_pos(self):
         if self.current_file_index != self.amount_of_entries-1:
             self.current_file_index += 1
-            try:
-                self.GUI.curr_file_name.set(self.file_entries[self.current_file_index])
-                self.GUI.file_label_status.set(self.file_label_statuses[self.file_entries[self.current_file_index]])
-            except IndexError:
-                pass
+            self.GUI.curr_file_name.set(self.file_entries[self.current_file_index])
+            self.GUI.file_label_status.set(self.file_label_statuses[self.file_entries[self.current_file_index]])
  
     def approve(self):
         '''Decides if the drawn changes will be applied'''
         cv2.imwrite(self.file_entries[self.current_file_index], self.file_being_read)
         # update file status to indicate it has been drawn over
         self.file_label_statuses[self.file_entries[self.current_file_index]] = "True"
-
         self.GUI.file_label_status.set(self.file_label_statuses[self.file_entries[self.current_file_index]])
+        dump_dict_to_json(self.json_file, self.file_label_statuses)
 
     def reject(self):
         '''Rejects the applied changes and falls back to a backup copy'''
         self.file_being_read = self.backup_file_cpy
-        self.file_label_statuses[self.file_entries[self.current_file_index]] = "False"
 
-        self.GUI.file_label_status.set(self.file_label_statuses[self.file_entries[self.current_file_index]])
+        
+        set_label = self.file_label_statuses[self.file_entries[self.current_file_index]] = "False"
 
-
+        self.GUI.file_label_status.set(set_label)
