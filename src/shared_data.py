cbbce14edc7c80a2b5c9cc16548eed56e2503a4b
Estudiante <apaapf23@gmail.com>
2026-03-06 18:53:06 -0300

    add updated labels

--- src/shared_data.py
+++ src/shared_data.py
@@ -10,17 +10,24 @@ class Comms:
 
         self.directory = tkinter.StringVar(value="")
         self.current_file_label = tkinter.StringVar(value="current file: ")
-        self.curr_file_label_status = tkinter.StringVar(value="current label status: ")
+        self.curr_file_label_status = tkinter.StringVar(value="was file labeled: ")
+        self.curr_folder_label = tkinter.StringVar(value="current folder: ")
 
         self.curr_file_name = tkinter.StringVar(value="")
         self.file_label_status = tkinter.StringVar(value="")
+        self.curr_folder = tkinter.StringVar(value="")
         self.directory = tkinter.StringVar(value="")
 
         self.scroll_text = scrolledtext.ScrolledText(self.gui, wrap='none')
         self.scroll_text.place(width=150, height=300, x=10, y=10)
 
-        self.menu_items_gui = {"curr_file": self.current_file_label, "filename": self.curr_file_name, 
-                "label": self.file_label_status, "curr_label": self.curr_file_label_status}
+        self.menu_items_gui = {"curr_file": self.current_file_label,
+                "filename": self.curr_file_name, 
+                "label": self.file_label_status, 
+                "curr_label": self.curr_file_label_status,
+                "curr_folder_label": self.curr_folder_label,
+                "curr_folder": self.curr_folder
+                }
 
     def req(self, order, sender):
         if sender == "gui":
